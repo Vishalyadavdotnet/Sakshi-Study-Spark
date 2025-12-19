0c0bf323029a3ea8bd411a86e52b55c58697d563
Vishal Yadav <vishalyadavdotnet@gmail.com>
2025-12-19 20:15:17 +0530

    dvbn

--- src/data/studyData.ts
+++ src/data/studyData.ts
@@ -140,6 +140,10 @@ export const studyData: StudySession[] = [
   { date: "2025-12-13", startTime: "19:00", endTime: "20:00", hours: 1, subject:  "English", chapter: "11", notes: "Lecture" },  
   { date: "2025-12-13", startTime: "20:00", endTime: "21:00", hours: 1, subject:  "Math", chapter: "11", notes: "Lecture" },  
   { date: "2025-12-14", startTime: "15:00", endTime: "16:00", hours: 1, subject:  "Sst", chapter: "16", notes: "Learn" },  
+  { date: "2025-12-18", startTime: "20:00", endTime: "21:00", hours: 1, subject:  "Science", chapter: "11", notes: "Learn" },  
+  { date: "2025-12-19", startTime: "15:30", endTime: "16:30", hours: 1, subject:  "Sst", chapter: "16", notes: "Learn" },  
+  { date: "2025-12-19", startTime: "18:00", endTime: "20:00", hours: 2, subject:  "Sst", chapter: "16", notes: "Learn" },  
+  { date: "2025-12-19", startTime: "20:00", endTime: "21:00", hours:  1, subject:  "Math", chapter: "11", notes: "Lecture" },  
    
    
    
