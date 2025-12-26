3547f07a5915b80c3a054d33886b7d6454b85970
Vishal Yadav <vishalyadavdotnet@gmail.com>
2025-12-26 20:23:50 +0530

    ghjk,

--- src/data/studyData.ts
+++ src/data/studyData.ts
@@ -161,6 +161,13 @@ export const studyData: StudySession[] = [
   { date: "2025-12-23", startTime: "18:00", endTime: "19:00", hours:  1, subject:  "Science", chapter: "9", notes: "Learn" },    
   { date: "2025-12-23", startTime: "19:00", endTime: "20:00", hours:  1, subject:  "English", chapter: "8", notes: "H.W" },    
   { date: "2025-12-23", startTime: "20:00", endTime: "21:00", hours:  1, subject:  "Math", chapter: "11", notes: "Lecture" },    
+  { date: "2025-12-24", startTime: "15:30", endTime: "16:30", hours:  1, subject:  "Sst", chapter: "11", notes: "Lecture" },    
+  { date: "2025-12-24", startTime: "19:00", endTime: "20:00", hours:  1, subject:  "English", chapter: "8", notes: "H.W" },    
+  { date: "2025-12-24", startTime: "20:00", endTime: "21:00", hours:  1, subject:  "Math", chapter: "12", notes: "Revision" },    
+  { date: "2025-12-25", startTime: "10:00", endTime: "11:00", hours:  1, subject:  "Sst", chapter: "22", notes: "Revision" },    
+  { date: "2025-12-26", startTime: "16:00", endTime: "17:00", hours:  1, subject:  "Sst", chapter: "22", notes: "Revision" },    
+  { date: "2025-12-26", startTime: "16:00", endTime: "17:00", hours:  1, subject:  "Sst", chapter: "22", notes: "Revision" },    
+  { date: "2025-12-26", startTime: "20:00", endTime: "21:00", hours:  1, subject:  "Math", chapter: "12", notes: "Revision" },    
    
    
    
